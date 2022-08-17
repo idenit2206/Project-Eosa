7acdcc164096139d90e2c5e0a78ec4ac6799ff5a
idenit2206 <idenit2206@iden-it.com>
2022-08-17 09:06:34 +0900

    CompanysCategory Entity 추가

--- src/main/java/com/eosa/web/companys/CompanysCategory.java
+++ src/main/java/com/eosa/web/companys/CompanysCategory.java
@@ -1,5 +1,25 @@
-package com.eosa.web.companys;
+package com.eosa.admin.companysmanager;
 
-public enum CompanysCategory {
-    HOME, INDUSTRY, SAFE, CIVIL, CYBER, PROPERTY, MEDI
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.eosa.web.companys.Companys;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="CompanysCategory")
+public class CompanysCategory {
+    
+    private Long companysCategoryIdx;
+    private Long companysIdx;
+    private String companysCategoryValue;
+
+    @ManyToOne
+    @JoinColumn(name="companysIdx")
+    private Companys companys;
+
 }
