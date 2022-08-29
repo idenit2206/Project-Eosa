a2d090b8c9921ddb00725359103e6ea3ce879b9b
idenit2206 <idenit2206@iden-it.com>
2022-08-29 20:22:51 +0900

    [서비스페이지] 회원정보 수정 완료

--- src/main/java/com/eosa/web/users/controller/MypageController.java
+++ src/main/java/com/eosa/web/users/controller/MypageController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eosa.web.users.Users;
 import com.eosa.web.users.entity.FindByUsersAccountEntity;
 import com.eosa.web.users.service.UsersService;
 import com.eosa.web.util.CustomResponseData;
@@ -54,4 +55,44 @@ public class MypageController {
         return result;
     }
     
+
+    @PostMapping("/mypage/updateUserInfo")
+    public CustomResponseData updateUserInfo(
+        @RequestBody String param
+    ) {
+        CustomResponseData result = new CustomResponseData();
+        JsonObject jsonObject = (JsonObject) JsonParser.parseString(param).getAsJsonObject();
+        Users paramUsers = new Users();
+            paramUsers.setUsersIdx(Long.parseLong(jsonObject.get("usersIdx").getAsString()));
+            paramUsers.setUsersAccount(jsonObject.get("usersAccount").getAsString().toLowerCase());
+            paramUsers.setUsersPass(jsonObject.get("usersPass").getAsString());
+            paramUsers.setUsersName(jsonObject.get("usersName").getAsString());
+            paramUsers.setUsersNick(jsonObject.get("usersNick").getAsString());
+            paramUsers.setUsersPhone(jsonObject.get("usersPhone").getAsString());
+            paramUsers.setUsersEmail(jsonObject.get("usersEmail").getAsString());
+            paramUsers.setUsersRole(jsonObject.get("usersRole").getAsString().toUpperCase());
+            String prevUsersAge = jsonObject.get("usersAge").getAsString();
+            int usersAge = Integer.parseInt(prevUsersAge.substring(0, 2));
+            paramUsers.setUsersAge(usersAge);
+            paramUsers.setUsersRegion1(jsonObject.get("usersRegion1").getAsString());
+            paramUsers.setUsersRegion2(jsonObject.get("usersRegion2").getAsString());
+            
+            // log.debug("user: {}", paramUsers.toString());
+
+            int transaction = usersService.updateUserInfo(paramUsers);
+            // log.debug("result: {}", transaction);
+
+            if(transaction == 1) {
+                result.setStatusCode(HttpStatus.OK.value());
+                result.setResultItem("SUCCESS");
+                result.setResponseDateTime(LocalDateTime.now());
+            }
+            else {
+                result.setStatusCode(HttpStatus.OK.value());
+                result.setResultItem("FAILURE");
+                result.setResponseDateTime(LocalDateTime.now());
+            }
+
+            return result;
+    }
 }
