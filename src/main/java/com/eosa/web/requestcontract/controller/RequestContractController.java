2b97d31553872cf3bae5ac94b6da7d2d76e1f12f
idenit2206 <idenit2206@gmail.com>
2022-10-04 15:43:24 +0900

    companysIdx companys 조회 사업자등록일 추가

--- src/main/java/com/eosa/web/requestcontract/controller/RequestContractController.java
+++ src/main/java/com/eosa/web/requestcontract/controller/RequestContractController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,13 @@ public class RequestContractController {
             result.setResultItem(null);
             result.setResponseDateTime(LocalDateTime.now());
         }
+
+        return result;
+    }
+
+    @PostMapping("/insertRequestContract")
+    public CustomResponseData insertRequestContract() {
+        CustomResponseData result = new CustomResponseData();
 
         return result;
     }
